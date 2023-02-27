6efb83134dc267678b584aced7205c7fec109ff3
Junaid Sandhu <junaidsandhu21@gmail.com>
2023-02-27 20:12:42 +0500

    Added variable for the action type

--- usereducerhook/src/App.js
+++ usereducerhook/src/App.js
@@ -1,35 +1,42 @@
-import React, { useReducer } from "react";
+import React, {useReducer} from "react";
 
-const App = () => {
-  const reducer = (state, action) => {
+
+const ACTION_REDUCER = {
+    INCREMENT: "increment",
+    DECREMENT: "decrement",
+}
+const reducer = (state, action) => {
     switch (action.type) {
-      case "Increment":
-        return { count: state.count + 1 };
-      case "Decrement":
-        return { count: state.count - 1 };
-      default:
-        return state.hello;
+        case ACTION_REDUCER.INCREMENT:
+            return {count: state.count + 1};
+        case ACTION_REDUCER.DECREMENT:
+            return {count: state.count - 1};
+        default:
+            return state.hello;
     }
-  };
-
-  const [state, dispatch] = useReducer(reducer, {
-    count: 0,
-    hello: "presses first button",
-  });
-  const increment = () => {
-    dispatch({ type: "Increment" });
-  };
-  const decrement = () => {
-    dispatch({ type: "Decrement" });
-  };
-
-  return (
-    <>
-      <button onClick={increment}>+</button>
-      {state.count === 0 ? state.hello : state.count}
-      <button onClick={decrement}>-</button>
-    </>
-  );
 };
 
-export default App;
+
+export default function App() {
+
+
+    const [state, dispatch] = useReducer(reducer, {
+        count: 0,
+        hello: "presses first button",
+    });
+    const increment = () => {
+        dispatch({type: ACTION_REDUCER.INCREMENT});
+    };
+    const decrement = () => {
+        dispatch({type: ACTION_REDUCER.DECREMENT});
+    };
+
+    return (
+        <>
+            <button onClick={increment}>+</button>
+            {state.count === 0 ? state.hello : state.count}
+            <button onClick={decrement}>-</button>
+        </>
+    );
+};
+
